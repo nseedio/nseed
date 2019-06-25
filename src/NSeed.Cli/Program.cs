f0e35dc23f5298040cb1007ab3a39cf77c6f46a7
Andrej Matijevic <Andrej.Matijevic@dreamlines.de>
2019-06-25 12:39:32 +0200

    feat: Add NSeed.Cli empty project with unit and integration tests

---
+++ src/NSeed.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSeed.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
