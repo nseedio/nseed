e66c003d3a9f4ade5fe5c28d2304aa0b475dffb5
Igor Rončević <ironcev@hotmail.com>
2019-06-25 22:31:39 +0200

    feat: Add AssemblyInfo to NSeed.Cli

---
+++ src/NSeed.Cli/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("NSeed.Cli.Tests.Unit")]
+// ReSharper disable CheckNamespace
+internal static partial class AssemblyDescription
+{
+    public const string Title = "NSeed CLI";
+
+    public const string Description = "NSeed CLI tool.";
+
+    public const string InformationalVersion = Version;
+}
+// ReSharper restore CheckNamespace
