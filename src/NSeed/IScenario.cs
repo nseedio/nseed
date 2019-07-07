ecd8c2f2eae61cffa8fe18a365577dea31786291
Igor Rončević <ironcev@hotmail.com>
2019-07-07 13:50:00 +0200

    feat: Add Requires attribute

--- src/NSeed/IScenario.cs
+++ src/NSeed/IScenario.cs
@@ -7,8 +7,10 @@
     /// scenarios to simulate different sales seasons.
     /// </summary>
     /// <remarks>
-    /// Scenarios do not have their own implementation. A concrete scenario is an
-    /// empty non-abstract class marked with this interface.
+    /// Scenarios do not have their own implementations. A concrete scenario is an
+    /// empty non-abstract class marked with this interface. The seeds and other
+    /// scenarios that are part of a scenario are defined by annotating that empty
+    /// scenario class with the <see cref="RequiresAttribute"/>.
     /// </remarks>
     public interface IScenario { }
 }
---
+++ src/NSeed/Requires.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NSeed
+{
+    /// <summary>
+    /// Specifies that a seedable class (a class that implements <see cref="ISeed"/> ot <see cref="IScenario"/>)
+    /// requires yield of another seedable class.
+    /// <br/>
+    /// The seedable class annotated with this attribute requires yield of the <see cref="ISeed"/>
+    /// or <see cref="IScenario"/> of the type <see cref="SeedableType"/>.
+    /// </summary>
+    /// <remarks>
+    /// This attribute should only be applied on seedable classes
+    /// (classes that implement <see cref="ISeed"/> or <see cref="IScenario"/>).
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public sealed class RequiresAttribute : Attribute
+    {
+        /// <summary>
+        /// The <see cref="Type"/> of a single <see cref="ISeed"/> or <see cref="IScenario"/>
+        /// whose yield is required by the seedable class annotated with this attribute.
+        /// </summary>
+        public Type SeedableType { get; }
+
+        /// <summary>
+        /// Creates new <see cref="RequiresAttribute"/> that specifies that a seedable class
+        /// requires yield of an <see cref="ISeed"/> or <see cref="IScenario"/> of the type <paramref name="seedableType"/>.
+        /// </summary>
+        public RequiresAttribute(Type seedableType) => SeedableType = seedableType;
+    }
+}
