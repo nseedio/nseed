6c84dcc4dd759bc5eb64f7206f64040d3db90183
Andrej Matijevic <andrej.matijevic@gmail.com>
2019-08-25 15:52:15 +0200

    test: Add installation of NSeed tool so that command nseed is globally available

--- src/NSeed.Cli.Tests.Integration/NewSubcommandTest.cs
+++ src/NSeed.Cli.Tests.Integration/NewSubcommandTest.cs
@@ -16,19 +16,28 @@ namespace NSeed.Cli.Tests.Integration
         public void Test()
         {
             TestRunner runner = new TestRunner();
-            var blahh = Assembly.GetExecutingAssembly();
-            var response = runner.Bla(@"C:/Repositories/Osobno/25.06.2019_NSeed/src/NSeed.Cli/bin/Debug/netcoreapp2.2/", new string[] { "NSeed.Cli.dll", "--help" });
+            var currentAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            var srcFolderPath = Path.GetFullPath(Path.Combine(currentAssemblyPath, "..", "..", "..", "..", ".."));
+            var toolNupkgPath = Path.Combine(srcFolderPath, "NSeed.Cli", "bin", "Debug");
+            // dotnet tool install --global --add-source C:\Repositories\Osobno\25.06.2019_NSeed\src\NSeed.Cli\bin\Debug NSeed.Cli
+            var installToolResponse = runner.Run(srcFolderPath, new string[]
+            {
+                $"dotnet tool install --global --add-source {toolNupkgPath} NSeed.Cli"
+            });
+
+            var response = runner.Run(srcFolderPath, new string[] { "nseed" });
             if (response.IsSuccess)
             {
+                runner.Run(srcFolderPath, new string[] { "dotnet tool uninstall --global NSeed.Cli" });
             }
         }
     }
 
-    internal class TestRunner : Runners.DotNetRunner
+    internal class TestRunner : DotNetRunner
     {
-        public RunStatus Bla(string workingDirectory, string[] arguments)
+        public new RunStatus Run(string workingDirectory, string[] arguments)
         {
-            return Run(workingDirectory, arguments);
+            return base.Run(workingDirectory, arguments);
         }
     }
 }
