ac953ac1b978a8629dc8c6657422e0bb57689a6c
Igor Rončević <ironcev@hotmail.com>
2019-08-25 11:49:59 +0200

    lab: Print final seed bucket info in Writing Grids on the Console

--- lab/WritingGridsOnTheConsole/CsConsoleFormat/NSeed/SeedBucket.cs
+++ lab/WritingGridsOnTheConsole/CsConsoleFormat/NSeed/SeedBucket.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using Alba.CsConsoleFormat;
 using static System.ConsoleColor;
@@ -16,15 +17,30 @@ namespace NSeed
             Console.WriteLine($"Is console available: {isConsoleAvailable}");
 
             // Standard behavior if the console is available.
-            Action initializeConsole = () => Console.OutputEncoding = Encoding.UTF8;
-            Func<Rect?> getRenderRect = () => null; // Default will be used.
-            Action parkCursor = () => ConsoleRenderer.ConsoleCursorPosition = new Point(0, ConsoleRenderer.ConsoleCursorPosition.Y);
+            Action initializeConsole = () =>
+            {
+                // According to CsConsoleFormat docummentation,
+                // if OS is Windows we have to set encoding to UTF8.
+                // See: https://github.com/Athari/CsConsoleFormat#getting-started
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    Console.OutputEncoding = Encoding.UTF8;
+            };
+            Rect? renderRect = new Rect(0, 0, Math.Min(Console.BufferWidth, 150), Size.Infinity);
+            var headerStroke = new LineThickness(LineWidth.None, LineWidth.None, LineWidth.None, LineWidth.Single);
+            bool underlineGridHeader = false;
+            bool writeFinalLineTerminator = true;
+            var foregroundColor = Console.ForegroundColor;
+            var backgroundColor = Console.BackgroundColor;
 
             if (!isConsoleAvailable)
             {
                 initializeConsole = () => { };
-                getRenderRect = () => new Rect(0, 0, 72, Size.Infinity);
-                parkCursor = () => { };
+                renderRect = new Rect(0, 0, 72, Size.Infinity);
+                headerStroke = LineThickness.None;
+                underlineGridHeader = true;
+                writeFinalLineTerminator = false;
+                foregroundColor = Gray;
+                backgroundColor = Black;
             }
 
             initializeConsole();
@@ -68,19 +84,16 @@ namespace NSeed
                 },
             };
 
-            ConsoleRenderer.RenderDocument(GenerateDocument("Seeds"), null, getRenderRect());
-
+            Console.WriteLine();
             Console.WriteLine();
 
-            ConsoleRenderer.RenderDocument(GenerateDocument("Scenarios"), null, getRenderRect());
+            ConsoleRenderer.RenderDocument(GenerateSummary(), null, renderRect);
 
-            Console.WriteLine();
+            ConsoleRenderer.RenderDocument(GenerateSeeds(), null, renderRect);
 
-            ConsoleRenderer.RenderDocument(GenerateDocument("Something"), null, getRenderRect());
+            ConsoleRenderer.RenderDocument(GenerateScenarios(), null, renderRect);
 
-            Console.WriteLine();
-
-            parkCursor();
+            if (writeFinalLineTerminator) Console.WriteLine();
 
             bool IsPhysicalConsoleAvailable()
             {
@@ -104,57 +117,154 @@ namespace NSeed
                 return outputIsNotRedirected && physicalConsoleExists;
             }
 
-            Document GenerateDocument(string title)
+            Grid CreateHeaderGrid(string text)
             {
-                var lst = new LineThickness(LineWidth.None);
-
-                var headerDocument = new Document(
-                    new Span(title)
-                    )
+                return new Grid
                 {
-                    TextAlign = TextAlign.Center,
-                    Margin = new Thickness(0),
-                    Padding = new Thickness(0)
-                };
+                    Stroke = LineThickness.None,
 
+                    Columns =
+                    {
+                        new Column { Width = GridLength.Auto }
+                    },
+
+                    Children =
+                    {
+                        new Cell(text) { Stroke = headerStroke, TextWrap = TextWrap.NoWrap },
+                        underlineGridHeader
+                            ? new Cell(new string('=', text.Length)) { Stroke = LineThickness.None, TextWrap = TextWrap.NoWrap }
+                            : null
+                    }
+                };
+            }
+
+            Document GenerateSummary()
+            {
                 var doc = new Document(
-                    headerDocument,
-                    "\n",
+                    CreateHeaderGrid("Seed Bucket Summary"),
                     new Grid
                     {
-                        Margin = new Thickness(2, 0, 2, 0),
-
+                        Margin = new Thickness(1, 0, 1, 2),
+                       
+                        Stroke = LineThickness.None,
+                        
                         Columns =
                         {
-                        new Column { Width = GridLength.Star(3) },
-                        new Column { Width = GridLength.Star(4) },
-                        new Column { Width = GridLength.Star(6) }
+                            new Column { Width = GridLength.Auto },
+                            new Column { Width = GridLength.Auto }
                         },
 
                         Children = {
-                        new Cell("Name") { TextWrap = TextWrap.WordWrap },
-                        new Cell("Description") { TextWrap = TextWrap.WordWrap },
-                        new Cell("Creates entities") { TextWrap = TextWrap.WordWrap },
-                        seeds.Select(seed => new[] {
-                            new Cell {
-                                Color = seed.HasErrors ? Red : Gray,
-                                Children = { seed.Name }
-                            },
-                            new Cell {
-                                Color = seed.HasErrors ? Red : Gray,
-                                Children = { seed.Description }
-                            },
-                            new Cell {
-                                Color = seed.HasErrors ? Red : Gray,
-                                Children = { seed.CreatedEntities }
-                            }
-                        })
+                            new Cell("Name:") { Stroke = LineThickness.None, TextWrap = TextWrap.NoWrap },
+                            new Cell("Some seed bucket name") { Stroke = LineThickness.None, TextWrap = TextWrap.NoWrap, Padding = new Thickness(1, 0, 1, 0) },
+                            new Cell("Description:") { Stroke = LineThickness.None, TextWrap = TextWrap.NoWrap },
+                            new Cell("Some seed bucket description that can potentially be very long.") { Stroke = LineThickness.None, TextWrap = TextWrap.WordWrap, Padding = new Thickness(1, 0, 1, 0) },
+                            new Cell("Number of seeds:") { Stroke = LineThickness.None, TextWrap = TextWrap.NoWrap },
+                            new Cell(12) { Stroke = LineThickness.None, TextWrap = TextWrap.NoWrap, Padding = new Thickness(1, 0, 1, 0) },
+                            new Cell("Number of scenarios:") { Stroke = LineThickness.None, TextWrap = TextWrap.NoWrap },
+                            new Cell(15) { Stroke = LineThickness.None, TextWrap = TextWrap.NoWrap, Padding = new Thickness(1, 0, 1, 0) },
                         }
                     }
                 )
                 {
-                    Margin = new Thickness(0),
-                    Padding = new Thickness(0)
+                    Color = foregroundColor,
+                    Background = backgroundColor
+                };
+
+                return doc;
+            }
+
+            Document GenerateSeeds()
+            {
+                var doc = new Document(
+                    CreateHeaderGrid("Seeds"),
+                    new Grid
+                    {
+                        Margin = new Thickness(1, 0, 1, 1),
+
+                        Stroke = LineThickness.None,
+
+                        Columns =
+                        {
+                            new Column { Width = GridLength.Star(3) },
+                            new Column { Width = GridLength.Star(4) },
+                            new Column { Width = GridLength.Star(3) }
+                        },
+
+                        Children = {
+                            new Cell("Name") { Stroke = headerStroke, TextWrap = TextWrap.NoWrap },
+                            new Cell("Description") { Stroke = headerStroke, TextWrap = TextWrap.NoWrap },
+                            new Cell("Creates") { Stroke = headerStroke, TextWrap = TextWrap.NoWrap },
+                            seeds.Select(seed => new[] {
+                                new Cell {
+                                    Padding = new Thickness(1, 0, 1, 1),
+                                    Stroke = LineThickness.None,
+                                    Color = seed.HasErrors ? Red : foregroundColor,
+                                    Children = { seed.Name }
+                                },
+                                new Cell {
+                                    Padding = new Thickness(1, 0, 1, 1),
+                                    Stroke = LineThickness.None,
+                                    Color = seed.HasErrors ? Red : foregroundColor,
+                                    Children = { seed.Description }
+                                },
+                                new Cell {
+                                    Padding = new Thickness(1, 0, 1, 1),
+                                    Stroke = LineThickness.None,
+                                    Color = seed.HasErrors ? Red : foregroundColor,
+                                    Children = { seed.CreatedEntities }
+                                }
+                            })
+                        }
+                    }
+                )
+                {
+                    Color = foregroundColor,
+                    Background = backgroundColor
+                };
+
+                return doc;
+            }
+
+            Document GenerateScenarios()
+            {
+                var doc = new Document(
+                    CreateHeaderGrid("Scenarios"),
+                    new Grid
+                    {
+                        Margin = new Thickness(1, 0, 1, 0),
+
+                        Stroke = LineThickness.None,
+
+                        Columns =
+                        {
+                            new Column { Width = GridLength.Star(4) },
+                            new Column { Width = GridLength.Star(6) },
+                        },
+
+                        Children = {
+                            new Cell("Name") { Stroke = headerStroke, TextWrap = TextWrap.NoWrap },
+                            new Cell("Description") { Stroke = headerStroke, TextWrap = TextWrap.NoWrap },
+                            seeds.Select(seed => new[] {
+                                new Cell {
+                                    Padding = new Thickness(1, 0, 1, 1),
+                                    Stroke = LineThickness.None,
+                                    Color = seed.HasErrors ? Red : foregroundColor,
+                                    Children = { seed.Name }
+                                },
+                                new Cell {
+                                    Padding = new Thickness(1, 0, 1, 1),
+                                    Stroke = LineThickness.None,
+                                    Color = seed.HasErrors ? Red : foregroundColor,
+                                    Children = { seed.Description }
+                                }
+                            })
+                        }
+                    }
+                )
+                {
+                    Color = foregroundColor,
+                    Background = backgroundColor
                 };
 
                 return doc;
