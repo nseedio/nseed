5adfa5e50e907bed211c8df2cc38687c12bbe5f2
Igor Rončević <ironcev@hotmail.com>
2019-10-27 11:08:23 +0100

    fix: Fix merge conflict in NewSubcommandRunner.cs

--- src/NSeed.Cli/Subcommands/New/Runner/NewSubcommandRunner.cs
+++ src/NSeed.Cli/Subcommands/New/Runner/NewSubcommandRunner.cs
@@ -16,7 +16,7 @@ namespace NSeed.Cli.Subcommands.New.Runner
 
         private (bool IsSuccesful, string Message) AddTemplate(NewSubcommandRunnerArgs args)
         {
-            var arguments = new[] { "new --install", args.Template.Path };
+            var arguments = new[] { "new --install", args.Template.DirectoryName };
             return Response(RunDotNet(args.SolutionDirectory, arguments));
         }
 
@@ -29,7 +29,7 @@ namespace NSeed.Cli.Subcommands.New.Runner
 
         private (bool IsSuccesful, string Message) RemoveTemplate(NewSubcommandRunnerArgs args)
         {
-            string[] arguments = new[] { "new --uninstall", args.Template.Path };
+            string[] arguments = new[] { "new --uninstall", args.Template.DirectoryName };
             return Response(RunDotNet(args.SolutionDirectory, arguments));
         }
 
