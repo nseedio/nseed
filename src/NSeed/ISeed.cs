16570f88e998d36da78e87d894f9ea3f770a78d0
Igor Rončević <ironcev@hotmail.com>
2019-12-16 16:10:54 +0100

    docs: Properly use verb "yield" when commenting ISeed interface members

--- src/NSeed/ISeed.cs
+++ src/NSeed/ISeed.cs
@@ -9,17 +9,17 @@ namespace NSeed
 #pragma warning disable SA1402 // File may only contain a single type
 #pragma warning disable SA1618 // Generic type parameters should be documented
     /// <summary>
-    /// A <see cref="ISeed"/> that seeds three types of entities.
+    /// A <see cref="ISeed"/> that yields three types of entities.
     /// </summary>
     public interface ISeed<TEntity1, TEntity2, TEntity3> : ISeed { }
 
     /// <summary>
-    /// A <see cref="ISeed"/> that seeds two types of entities.
+    /// A <see cref="ISeed"/> that yields two types of entities.
     /// </summary>
     public interface ISeed<TEntity1, TEntity2> : ISeed { }
 
     /// <summary>
-    /// A <see cref="ISeed"/> that seeds entities of type <typeparamref name="TEntity"/>.
+    /// A <see cref="ISeed"/> that yields entities of type <typeparamref name="TEntity"/>.
     /// </summary>
     public interface ISeed<TEntity> : ISeed { }
 #pragma warning restore SA1618 // Generic type parameters should be documented
@@ -31,21 +31,21 @@ namespace NSeed
     /// <remarks>
     /// To enable better categorization of seeds, the best practice is to use
     /// one of the generic versions of the <see cref="ISeed"/> interface
-    /// that denote the type of entities that the seed seeds.
+    /// that denote the type of entities that the seed yields.
     /// <br/>
-    /// Ideally, a seed should seed entities of a single type.
+    /// Ideally, a seed should yield entities of a single type.
     /// Thus, the mostly used <see cref="ISeed"/> interface in your application
     /// should be <see cref="ISeed{TEntity}"/>.
     /// </remarks>
     public interface ISeed
     {
         /// <summary>
-        /// Seeds this seed.
+        /// Seeds this seed and produces its yield.
         /// </summary>
         Task Seed();
 
         /// <summary>
-        /// Returns true if the yield of this seed exists.
+        /// Returns true if the yield of this seed already exists.
         /// This means that the <see cref="Seed"/> method was already successfuly executed.
         /// </summary>
         Task<bool> HasAlreadyYielded();
