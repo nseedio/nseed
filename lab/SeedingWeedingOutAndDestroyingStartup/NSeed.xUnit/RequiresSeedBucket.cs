fbda5dd78cc489e984b00197512c67480de5955b
Igor Rončević <ironcev@hotmail.com>
2020-03-21 17:15:49 +0100

    lab: Refactor RequiresSeedBucket fixtures by adding base class

--- lab/SeedingWeedingOutAndDestroyingStartup/NSeed.xUnit/RequiresSeedBucket.cs
+++ lab/SeedingWeedingOutAndDestroyingStartup/NSeed.xUnit/RequiresSeedBucket.cs
@@ -1,5 +1,3 @@
-using NSeed.Discovery.SeedBucket.ReflectionBased;
-using NSeed.Seeding;
 using System;
 using System.Linq;
 using System.Reflection;
@@ -12,22 +10,12 @@ namespace NSeed.Xunit
     /// TODO.
     /// </summary>
     /// <typeparam name="TSeedBucket">TODO.TODO.</typeparam>
-    public class RequiresSeedBucket<TSeedBucket>
+    public class RequiresSeedBucket<TSeedBucket> : RequiresSeedBucketFixture
         where TSeedBucket : SeedBucket
     {
         public RequiresSeedBucket()
-            : this(typeof(TSeedBucket))
+            : base(typeof(TSeedBucket), null)
         {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RequiresSeedBucket{TSeedBucket}"/> class.
-        /// </summary>
-        /// <param name="seedBucketType">TODO.</param>
-        /// <param name="seedingStartupType">TODO.TODO.</param>
-        protected RequiresSeedBucket(Type seedBucketType)
-        {
-           RequiresSeedBucketHelper.SeedSeedBucket(seedBucketType, null);
         }
     }
 
@@ -37,48 +25,14 @@ namespace NSeed.Xunit
     /// <typeparam name="TSeedBucket">TODO.TODO.</typeparam>
     /// <typeparam name="TSeedingStartupType">TODO.TODO.TODO.</typeparam>
 #pragma warning disable SA1402 // File may only contain a single type
-    public class RequiresSeedBucket<TSeedBucket, TSeedingStartupType>
+    public class RequiresSeedBucket<TSeedBucket, TSeedingStartupType> : RequiresSeedBucketFixture
 #pragma warning restore SA1402 // File may only contain a single type
         where TSeedBucket : SeedBucket
         where TSeedingStartupType : SeedBucketStartup
     {
         public RequiresSeedBucket()
-            : this(typeof(TSeedBucket), typeof(TSeedingStartupType))
+            : base(typeof(TSeedBucket), typeof(TSeedingStartupType))
         {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RequiresSeedBucket{TSeedBucket, TSeedingStartupType}"/> class.
-        /// </summary>
-        /// <param name="seedBucketType">TODO.</param>
-        /// <param name="seedingStartupType">TODO.TODO.</param>
-        protected RequiresSeedBucket(Type seedBucketType, Type seedingStartupType)
-        {
-            RequiresSeedBucketHelper.SeedSeedBucket(seedBucketType, seedingStartupType);
-        }
-    }
-
-#pragma warning disable SA1402 // File may only contain a single type
-    internal static class RequiresSeedBucketHelper
-#pragma warning restore SA1402 // File may only contain a single type
-    {
-        internal static void SeedSeedBucket(Type seedBucketType, Type? seedingStartupType)
-        {
-            // TODO: Check not null, must be seedbucket.
-
-            var outputSink = new InternalQueueOutputSink();
-            var seedBucketInfoBuilder = new ReflectionBasedSeedBucketInfoBuilder();
-
-            var seeder = new Seeder(seedBucketInfoBuilder, outputSink);
-
-            // TODO: Is not null, IsSeedBucketSeedingType etc.
-
-            var seedingReport = seedingStartupType is null
-                ? seeder.SeedSeedBucket(seedBucketType).Result
-                : seeder.SeedSeedBucket(seedBucketType, seedingStartupType).Result;
-
-            // TODO: See the best way to get the error message, inner exception and those kind of things. Throw some rich exception accordingly e.g. SeedingSingleSeedFaildException.
-            if (seedingReport.Status != SeedingStatus.Succeeded) throw new Exception($"Seeding failed.{Environment.NewLine}{outputSink.GetOutputAsString()}");
         }
     }
 }
---
+++ lab/SeedingWeedingOutAndDestroyingStartup/NSeed.xUnit/RequiresSeedBucketFixture.cs
@@ -0,0 +1,28 @@
+using NSeed.Discovery.SeedBucket.ReflectionBased;
+using NSeed.Seeding;
+using System;
+
+namespace NSeed.Xunit
+{
+    public abstract class RequiresSeedBucketFixture
+    {
+        protected RequiresSeedBucketFixture(Type seedBucketType, Type? seedingStartupType)
+        {
+            // TODO: Check not null, must be seedbucket.
+
+            var outputSink = new InternalQueueOutputSink();
+            var seedBucketInfoBuilder = new ReflectionBasedSeedBucketInfoBuilder();
+
+            var seeder = new Seeder(seedBucketInfoBuilder, outputSink);
+
+            // TODO: Is not null, IsSeedBucketSeedingType etc.
+
+            var seedingReport = seedingStartupType is null
+                ? seeder.SeedSeedBucket(seedBucketType).Result
+                : seeder.SeedSeedBucket(seedBucketType, seedingStartupType).Result;
+
+            // TODO: See the best way to get the error message, inner exception and those kind of things. Throw some rich exception accordingly e.g. SeedingSingleSeedFaildException.
+            if (seedingReport.Status != SeedingStatus.Succeeded) throw new Exception($"Seeding failed.{Environment.NewLine}{outputSink.GetOutputAsString()}");
+        }
+    }
+}
