afbb481d4cb8e3ea80709733bed0f997b2d15d8e
Igor Rončević <ironcev@hotmail.com>
2020-04-10 22:53:14 +0200

    lab: Move new ReleaseNotes infrastructure to the lab project

--- lab/SeedingWeedingOutAndDestroyingStartup/NSeed/Properties/AssemblyInfo.cs
+++ lab/SeedingWeedingOutAndDestroyingStartup/NSeed/Properties/AssemblyInfo.cs
@@ -4,6 +4,3 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("NSeed.Tests.Unit")]
 [assembly: InternalsVisibleTo("NSeed.Xunit")]
 [assembly: InternalsVisibleTo("NSeed.Sdk")]
-
-[assembly: AssemblyTitle("NSeed")]
-[assembly: AssemblyDescription("Data seeding framework for .NET.")]
